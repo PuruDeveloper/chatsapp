1059d098d77f966e888459889b1381ccd7c438f8
PuruDeveloper <78719151+PuruDeveloper@users.noreply.github.com>
2021-07-04 18:25:58 +0530

    Added firebase.js, Sidebar component and cleaned app from initial recat template

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAktybIsUcrnt47HchfY5Qoibvt1ZcidJY",
+  authDomain: "whatsapp-clone-july-2021.firebaseapp.com",
+  projectId: "whatsapp-clone-july-2021",
+  storageBucket: "whatsapp-clone-july-2021.appspot.com",
+  messagingSenderId: "756486229817",
+  appId: "1:756486229817:web:866cb9574eb5833ed062e8",
+  measurementId: "G-ZXRD7WE66F",
+};
