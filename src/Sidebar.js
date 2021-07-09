39d842ff36992f627711ce37a24d5036371ab557
PuruDeveloper <78719151+PuruDeveloper@users.noreply.github.com>
2021-07-09 19:47:31 +0530

    Adding enhancements to sidebar. Added sidebar-header, search bar.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,38 @@
 import React from "react";
 import "./Sidebar.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLagreIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { SearchOutlined } from "@material-ui/icons";
+import SidebarChat from "./SidebarChat";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <div className="sidebar__header"></div>
-      <div className="sidebar__search"></div>
-      <div className="sidebar__chats"></div>
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLagreIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input placeholder="Search or start new chat" type="text" />
+        </div>
+      </div>
+      <div className="sidebar__chats">
+        <SidebarChat />
+      </div>
     </div>
   );
 }
---
+++ src/SidebarChat.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SidebarChat.css";
+
+function SidebarChat() {
+  return (
+    <div classname="sidebarChat">
+      <h1>hey</h1>
+    </div>
+  );
+}
+
+export default SidebarChat;
