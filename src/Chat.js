06dddc761b9628b03e98c0c9445be4ed19f83a82
PuruDeveloper <78719151+PuruDeveloper@users.noreply.github.com>
2021-07-20 18:26:43 +0530

    Added a chat page to show individual group chats. Also added css to this chat component. Next will attach it to firebase dabase.

---
+++ src/Chat.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import "./Chat.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import { AttachFile, SearchOutlined } from "@material-ui/icons";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
+import MoreVert from "@material-ui/icons/MoreVert";
+
+function Chat() {
+  const [seed, setSeed] = useState("");
+  const [input, setInput] = useState("");
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    setInput("");
+  };
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at ...</p>
+        </div>
+
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body">
+        <p className={`chat__message ${true && "chat__reciever"}`}>
+          <span className="chat__name">Puru</span>
+          Hey
+          <span className="chat__timestamp">3:52pm</span>
+        </p>
+      </div>
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form type="submit">
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Type your message..."
+            type="text"
+          />
+          <button onClick={sendMessage} type="submit">
+            Send a message
+          </button>
+        </form>
+        <MicIcon />
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
